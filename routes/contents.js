ffafcfc6746dfcf4146dc51087335dbc61722f16
snakazawa <nakazawa.tms0314@gmail.com>
2016-07-30 01:36:19 +0900

    投票する手間ぐらいまではできた

--- routes/contents.js
+++ routes/contents.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../lib/models/user');
+var Team = require('../lib/models/team');
+var VoteItem = require('../lib/models/vote_item');
 
 router.get('/userpage', function (req, res) {
     var username = req.user ? req.user.username : null;
@@ -9,6 +11,27 @@ router.get('/userpage', function (req, res) {
         displayTitle: 'ユーザページ',
         username: username,
         isAdmin : User.isAdminUser(username)
+    });
+});
+
+router.get('/vote', function (req, res, next) {
+    var username = req.user ? req.user.username : null;
+
+    Team.getWithoutMe(username, function (err, voteTeams) {
+        if (err) { return next(err); }
+
+        VoteItem.find({enabled: true}, function (err, voteItems) {
+            if (err) { return next(err); }
+
+            res.render('vote', {
+                title: '審査',
+                displayTitle: '審査',
+                username: username,
+                voteTeams: voteTeams,
+                voteItems: voteItems,
+                isAdmin : User.isAdminUser(username)
+            });
+        });
     });
 });
 
